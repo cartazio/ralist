c43326eeb87b2fd6826d3409bc1bbf2e8b4916a0
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2020-11-18 19:26:10 -0600

    more cleanups!

--- benchmark/benchmarking.hs
+++ benchmark/benchmarking.hs
@@ -25,25 +25,25 @@ main = defaultMain [
     bgroup "drop"
         [ bench "TenThousand" $ whnf (Data.RAList.drop 100) tenThousand,
           bench "HundredThousand" $ whnf (Data.RAList.drop 100) hundredThousand,
-          bench "Million" $ whnf (Data.RAList.drop 100) million,
-          bench "TenMillion" $ whnf (Data.RAList.drop 100) tenMillion,
+          --bench "Million" $ whnf (Data.RAList.drop 100) million,
+          --bench "TenMillion" $ whnf (Data.RAList.drop 100) tenMillion,
 
           bench "TenThousand-Drop1" $ whnf (Data.RAList.drop 1) tenThousand,
-          bench "HundredThousand-Drop1" $ whnf (Data.RAList.drop 1) hundredThousand,
-          bench "Million-Drop1" $ whnf (Data.RAList.drop 1) million,
-          bench "TenMillion-Drop1" $ whnf (Data.RAList.drop 1) tenMillion
+          bench "HundredThousand-Drop1" $ whnf (Data.RAList.drop 1) hundredThousand
+          --bench "Million-Drop1" $ whnf (Data.RAList.drop 1) million,
+          --bench "TenMillion-Drop1" $ whnf (Data.RAList.drop 1) tenMillion
         ],
 
     bgroup "simpleDrop"
         [ bench "TenThousand" $ whnf (Data.RAList.simpleDrop 100) tenThousand,
           bench "HundredThousand" $ whnf (Data.RAList.simpleDrop 100) hundredThousand,
-          bench "Million" $ whnf (Data.RAList.simpleDrop 100) million,
-          bench "TenMillion" $ whnf (Data.RAList.simpleDrop 100) tenMillion,
+          --bench "Million" $ whnf (Data.RAList.simpleDrop 100) million,
+          --bench "TenMillion" $ whnf (Data.RAList.simpleDrop 100) tenMillion,
 
           bench "TenThousand-Drop1" $ whnf (Data.RAList.simpleDrop 1) tenThousand,
-          bench "HundredThousand-Drop1" $ whnf (Data.RAList.simpleDrop 1) hundredThousand,
-          bench "Million-Drop1" $ whnf (Data.RAList.simpleDrop 1) million,
-          bench "TenMillion-Drop1" $ whnf (Data.RAList.simpleDrop 1) tenMillion
+          bench "HundredThousand-Drop1" $ whnf (Data.RAList.simpleDrop 1) hundredThousand
+          --bench "Million-Drop1" $ whnf (Data.RAList.simpleDrop 1) million,
+          --bench "TenMillion-Drop1" $ whnf (Data.RAList.simpleDrop 1) tenMillion
         ],
 
     bgroup "cons"
@@ -57,7 +57,7 @@ main = defaultMain [
         ],
     bgroup "lookup last element"
         [ bench "TenThousand" $ whnf  (tenThousand Data.RAList.!!) 10000,
-          bench "HundredThousand" $ whnf (hundredThousand Data.RAList.!!) 100000,
-          bench "Million" $ whnf (million Data.RAList.!!) 1000000,
-          bench "TenMillion" $ whnf (tenMillion Data.RAList.!!) 10000000
+          bench "HundredThousand" $ whnf (hundredThousand Data.RAList.!!) 100000
+          --bench "Million" $ whnf (million Data.RAList.!!) 1000000,
+          --bench "TenMillion" $ whnf (tenMillion Data.RAList.!!) 10000000
         ] ]
