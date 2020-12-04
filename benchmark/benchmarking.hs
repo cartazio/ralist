087e4ff10106a2bf2d6dc7c3fdbc88f1955c91db
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2020-12-04 09:38:36 -0500

    how old will it build?

--- benchmark/benchmarking.hs
+++ benchmark/benchmarking.hs
@@ -3,23 +3,26 @@ module Main where
 import Criterion.Main
 import Data.RAList
 
+{-# NOINLINE hundred #-}
 hundred :: RAList Int
 hundred = fromList [0..100]
 
+{-# NOINLINE thousand#-}
 thousand :: RAList Int
 thousand = fromList [0..1000]
 
+{-# NOINLINE tenThousand#-}
 tenThousand :: RAList Int
 tenThousand = fromList [0..10000]
 
 hundredThousand :: RAList Int
 hundredThousand = fromList [0..100000]
 
-million :: RAList Int
-million = fromList [0..1000000]
+--million :: RAList Int
+--million = fromList [0..1000000]
 
-tenMillion :: RAList Int
-tenMillion = fromList [0..10000000]
+--tenMillion :: RAList Int
+--tenMillion = fromList [0..10000000]
 
 main = defaultMain [
     bgroup "drop"
@@ -39,16 +42,16 @@ main = defaultMain [
         ],
 
     bgroup "cons"
-        [ bench "hundred" $ whnf (Data.RAList.cons 0) hundred,
-          bench "thousand" $ whnf (Data.RAList.cons 0) thousand
+        [ bench "hundred" $ whnf (Data.RAList.cons 0) hundred
+          ,bench "thousand" $ whnf (Data.RAList.cons 0) thousand
         ],
 
     bgroup "uncons"
-        [ bench "hundred" $ whnf Data.RAList.uncons hundred,
-          bench "thousand" $ whnf Data.RAList.uncons thousand
+        [ bench "hundred" $ whnf Data.RAList.uncons hundred
+          ,bench "thousand" $ whnf Data.RAList.uncons thousand
         ],
     bgroup "lookup last element"
-        [ bench "TenThousand" $ whnf  (tenThousand Data.RAList.!!) 10000,
-          bench "HundredThousand" $ whnf (hundredThousand Data.RAList.!!) 100000
+        [ bench "TenThousand" $ whnf  (tenThousand Data.RAList.!!) 10000
+          --,bench "HundredThousand" $ whnf (hundredThousand Data.RAList.!!) 100000
 
         ] ]
